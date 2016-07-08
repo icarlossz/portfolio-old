c4f3ae62ff724b5573fe5f8dacb678a535192098
Carlos Suarez <sz.carlos@hotmail.com>
2016-07-08 10:51:39 -0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $("#menu-ham").on('click', function () {
-    $('#menu').addClass('hide');
-    $('#menu').toggleClass('active');
-    $('#menu-bars').toggleClass('fa-bars fa-times');
-  })
-});
